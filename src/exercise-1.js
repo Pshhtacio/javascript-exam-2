5e1763dbd119ec377e1d8e61efb71277586a2d68
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-08 18:19:58 +0800

    (JavScript Exam 02) - Add unit test for excercise-1.js

--- src/exercise-1.js
+++ src/exercise-1.js
@@ -1,5 +1,11 @@
 const getCommonItems = (array1, array2) => {
-  // implement code here
+    return array1.reduce((commonItems, item) => {
+        if(array2.includes(item)){
+            commonItems.push(item);
+        };
+        return commonItems;
+    }, []);
 };
+
 
 export default getCommonItems;
